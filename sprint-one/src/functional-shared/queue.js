3d21341e060ac04ad7f8aa5ad7c6ee62dfb84980
rachelsison <rsison87@gmail.com>
2014-10-29 15:56:09 -0700

    functional-shared queue completed

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,4 +1,11 @@
 var makeQueue = function(){
+  var queueInstance = {};
+  queueInstance.tail =  -1;
+  queueInstance.head = 0;
+  queueInstance.count = 0;
+  queueInstance.storage = {};
+  _.extend(queueInstance, queueMethods);
+  return queueInstance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
@@ -6,4 +13,26 @@ var makeQueue = function(){
 var queueMethods = {};
 
 
+
+queueMethods.enqueue = function(value){
+    this.storage[++this.tail]= value;
+    this.count++;
+    return this.count;
+  };
+
+queueMethods.dequeue = function(){
+    if(this.count > 0){
+      var temp = this.storage[this.head];
+      delete this.storage[this.head];
+      this.head++;
+      this.count--;
+      return temp;
+    }else{
+      return;
+    }
+  };
+
+queueMethods.size = function(){
+    return this.count;
+  };
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackInstance = {};
+    stackInstance.storage = {};
+    stackInstance.count = 0;
+    _.extend(stackInstance, stackMethods)
+  return stackInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+};
+
+  stackMethods.push = function(value){
+    this.storage[this.count++] = value;
+    return this.count;
+  };
+
+  stackMethods.pop = function(){
+    if(this.count > 0){
+      var temp = this.storage[this.count - 1];
+      delete this.storage[--this.count];
+      return temp;
+    }else{
+      return;
+    }
+  };
+
+  stackMethods.size = function(){
+    return this.count;
+  };
 
 
