5225c47865c927a89c04f1e06f54cb70a81e8696
rachelsison <rsison87@gmail.com>
2014-10-30 11:03:44 -0700

    'Completed linkedList'

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(this.head === null && this.tail === null){
+      var tempNode = makeNode(value);
+      this.head = tempNode;
+      this.tail = tempNode;
+    }else{
+      var oldTail = this.tail;
+      this.tail = makeNode(value);
+      oldTail.next = this.tail;
+    }
   };
 
   list.removeHead = function(){
+    var tempNode = this.head;
+
+    if(this.head === null){
+      return;
+    }else if(this.head === this.tail){
+      this.head = null;
+      this.tail = null;
+    }else {
+      this.head = this.head.next;
+    }return tempNode.value;
   };
 
   list.contains = function(target){
+    var temp = this.head;
+    while(temp){
+      if (target === temp.value){
+        return true;
+      }
+      temp = temp.next;
+    }return false;
   };
 
   return list;
