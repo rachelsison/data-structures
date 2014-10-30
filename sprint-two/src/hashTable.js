c9f159faf1110eee4fc0702e44e0121c02e20bdf
rachelsison <rsison87@gmail.com>
2014-10-30 16:43:01 -0700

    Complete hashTables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,14 +5,30 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  var addition = {};
+  addition[k]= v;
+
+  if(this._storage.get(i) === undefined ){
+    this._storage.set(i, addition);
+  }else{
+    var temp =  this._storage.get(i);
+    temp = _.extend(temp, addition);
+    this._storage.set(i, temp );
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  return (this._storage.get(i))[k];
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var temp = this._storage.get(i);
+
+  temp[k] = null;
+  this._storage.set(i, temp);
 
 };
 
