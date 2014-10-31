15525d0e43c068d93144a47893753fe78915c3c4
rachelsison <rsison87@gmail.com>
2014-10-30 20:38:13 -0700

    Completed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,6 +2,8 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -11,14 +13,31 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  if(this.children === undefined){
+    this.children = [makeTree(value)];
+  }
+  else{
+    this.children.push(makeTree(value));
+  }
 };
 
 treeMethods.contains = function(target){
+  var result = false;
 
+  if(target === this.value){
+    result = true;
+  }
+  else if(this.children !== undefined){
+    for(var i=0; i<this.children.length; i++){
+      result = result || this.children[i].contains(target);
+    }
+  }
+  return result;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addChild === O(1);
+ * contains === O(n);
  */
