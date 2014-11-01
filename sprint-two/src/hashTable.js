74364cb12761bb45acf51c511aa1dcd122572e63
rachelsison <rsison87@gmail.com>
2014-10-31 20:29:48 -0700

    updated hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,30 +6,33 @@ var HashTable = function(){
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
-  var addition = {};
-  addition[k]= v;
+  var tuple = [k,v];
 
   if(this._storage.get(i) === undefined ){
-    this._storage.set(i, addition);
+    this._storage.set(i, [tuple]);
   }else{
-    var temp =  this._storage.get(i);
-    temp = _.extend(temp, addition);
-    this._storage.set(i, temp );
-  }
+    var bucket =  this._storage.get(i);
+    bucket.push(tuple);  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return (this._storage.get(i))[k];
+  var bucket = (this._storage.get(i));
+  for(var index=0; index < bucket.length; index++){
+    if(bucket[index][0] === k){
+      return bucket[index][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var temp = this._storage.get(i);
-
-  temp[k] = null;
-  this._storage.set(i, temp);
-
+  var bucket = this._storage.get(i);
+  for(var index=0; index < bucket.length; index++){
+    if(bucket[index][0] === k){
+      bucket[index][1] = null;
+    }
+  }
 };
 
 
